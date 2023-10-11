2b8dc60673e64d0f17bb27b8c7aff51625bfb09f
AntonDybko <dybko.anton11@ukr.net>
2023-10-11 21:55:48 +0200

    lab1

---
+++ lab1/src/Hello.java
@@ -0,0 +1,8 @@
+package Zad6;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Witaj Java!!!");
+    }
+    //jar cfvm hello.jar ../MANIFEST.MF Hello.class
+}
