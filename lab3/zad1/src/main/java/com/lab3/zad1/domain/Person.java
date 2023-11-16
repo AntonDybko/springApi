1a4580f0ac1c92113b3d47654c34aaf48e79d2c0
Anton Dybko <adybko@INF.UG.EDU.PL>
2023-11-16 17:01:12 +0100

    'lab3'

--- lab3/zad1/src/main/java/com/lab3/zad1/domain/Person.java
+++ lab3/zad1/src/main/java/com/lab3/zad1/domain/Person.java
@@ -16,6 +16,60 @@ public class Person {
         this.email = email;
         this.birthYear = birthYear;
     }
+    public Person(){
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
+    /*@Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, email, birthYear, super.hashCode());
+    }*/
 
     @Override
     public String toString() {
